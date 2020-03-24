f6f403639c8bda60791f7d646e63f7c73b3b460f
muellerda <david.muelle2@web.de>
2020-03-24 16:26:18 +0100

    Initial commit

---
+++ app/src/main/java/com/spotify/visualizer/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.spotify.visualizer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
+
+class MainActivity : AppCompatActivity() {
+
+    private val clientId = "ab8463ed17e14d2b84db69cc59bcadda"
+    private val redirectUri = "com.spotify.visualizer://callback"
+    private var spotifyAppRemote: SpotifyAppRemote? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Set the connection parameters
+        val connectionParams = ConnectionParams.Builder(clientId)
+            .setRedirectUri(redirectUri)
+            .showAuthView(true)
+            .build()
+
+        SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
+            override fun onConnected(appRemote: SpotifyAppRemote) {
+                spotifyAppRemote = appRemote
+                Log.d("MainActivity", "Connected! Yay!")
+                // Now you can start interacting with App Remote
+                connected()
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                Log.e("MainActivity", throwable.message, throwable)
+                // Something went wrong when attempting to connect! Handle errors here
+            }
+        })
+    }
+
+    private fun connected() {
+        // Play a playlist
+        spotifyAppRemote?.playerApi?.play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+    }
+}
+
